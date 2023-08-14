6c25f892c1d4118f02d626f20824872a6d4997ae
minis <minis>
2023-08-14 10:22:29 +0800

    oo

---
+++ build/precache-manifest.80391605588b9c17300938e41a4e0e02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c523e9d8b5f75b9daa7c",
+    "url": "/static/js/main.c523e9d8.chunk.js"
+  },
+  {
+    "revision": "4319bcf02f3280880484",
+    "url": "/static/js/1.4319bcf0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "954a2fb636c15a559d9f786321177cc6",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import ChatList from './ChatList';
 import AppBar from './AppBar';
 function App() {
+  const [userId, setUserId] = useState('');
+  const [publicKey, setPublicKey] = useState('');
+  useEffect(() => {
+    const storedUserId = localStorage.getItem('userId');
+    const storedPublicKey = localStorage.getItem('publicKey');
+
+    if (storedUserId && storedPublicKey) {
+      setUserId(storedUserId);
+      setPublicKey(storedPublicKey);
+    }
+  }, []);
+
+  useEffect(() => {
+    // 在这里执行轮询逻辑，使用 userId 和 publicKey
+    console.log('Start polling with userId:', userId);
+    console.log('Start polling with publicKey:', publicKey);
+
+    // 清理函数在组件卸载时会被调用
+    return () => {
+      // 在这里清除轮询定时器或取消其他轮询相关的操作
+      console.log('Stop polling');
+    };
+  }, [userId, publicKey]);
   return (
     <div>
 	<AppBar/>
