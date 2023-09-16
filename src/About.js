70fa87316a643c15d910f10fac62adbd4eeb092a
haha <ministarmail@msn.cn>
2023-09-16 22:38:30 +0800

    oo

--- src/About.js
+++ src/About.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import AppBar from './AppBar';
 function About() {
-  const cleanwhat = 'all';
   return (
     <div>
-	<AppBar cleanwhat={cleanwhat} />
 	<div>
 	  app icon from <a href="https://www.flaticon.com/free-icons/chat-box" title="chat box icons">Chat box icons created by Anggara - Flaticon</a>
+	  other icon from mui icons
 	</div>
     </div>
   )
